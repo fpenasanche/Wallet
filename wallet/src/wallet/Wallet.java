3369846df380b08cffa98c2905f6f898a4e37a1e
fpenasanche <62160032+fpenasanche@users.noreply.github.com>
2020-04-20 18:57:16 -0500

    Update Wallet.java

--- wallet/src/wallet/Wallet.java
+++ wallet/src/wallet/Wallet.java
@@ -1,11 +1,13 @@
 
 package wallet;
-
+ import javax.swing.JOptionPane;
 
 public class Wallet {
 
 
     public static void main(String[] args) {
+        
+        JOptionPane.showMessageDialog(null, "Hola Mundo");
   
     }
     
